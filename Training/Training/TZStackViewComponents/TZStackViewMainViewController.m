b4b328a6c24961144f3558023fd6addf51595db4
danielaRiesgo <danielap.riesgo@gmail.com>
2016-02-10 09:48:26 -0300

    Empezando con la stackView principal de la tercer parte del primer ejercicio. Puestas las vistas contenido más básicas posibles. Aún sin scrollView.

--- Training/Training/TZStackViewComponents/TZStackViewMainViewController.m
+++ Training/Training/TZStackViewComponents/TZStackViewMainViewController.m
@@ -28,38 +28,114 @@
 
 - (void)loadView {
     self.view = [[UIView alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
+    self.view.backgroundColor = [UIColor lightGrayColor];
     
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-    [label setBackgroundColor: [UIColor greenColor]];
-    label.text = @"Hola";
+//    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+//    [label setBackgroundColor: [UIColor greenColor]];
+//    label.text = @"Hola";
     
-    UIView *view1 = [[UIView alloc] init];
-    view1.backgroundColor = [UIColor blueColor];
-    [view1.heightAnchor constraintEqualToConstant:100].active = true;
-    [view1.widthAnchor constraintEqualToConstant:120].active = true;
+//    UIScrollView *scrollView = [[UIScrollView alloc] init];
+//    [self.view addSubview:scrollView];
+//    [scrollView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor];
+//    [scrollView.topAnchor constraintEqualToAnchor:self.view.topAnchor];
+//    [scrollView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor];
+//    [scrollView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor];
+//    scrollView.translatesAutoresizingMaskIntoConstraints = NO;
+
     
-    TZStackView * stackView = [[TZStackView alloc] initWithArrangedSubviews:@[label]]; //@[label]];
+    UIView *contentView = [[UIView alloc] init];
+    contentView.backgroundColor = [UIColor clearColor];
+    
+    [self.view addSubview:contentView];
+//    [scrollView addSubview:contentView];
+    [contentView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
+    [contentView.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
+    [contentView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
+    [contentView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
+    contentView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    
+    TZStackView *stackView = [[TZStackView alloc] initWithArrangedSubviews:@[[self createShopView], [self createProductsView], [self createAddressView], [self createPictureView], [self createClientView], [self createNotesView]]];
     
     stackView.axis = UILayoutConstraintAxisVertical;
     stackView.distribution = UIStackViewDistributionEqualSpacing;
-    stackView.alignment = UIStackViewAlignmentCenter;
-    stackView.spacing = 30;
-    [stackView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor];
-    [stackView.topAnchor constraintEqualToAnchor:self.view.topAnchor];
-    stackView.translatesAutoresizingMaskIntoConstraints = false;
+    stackView.alignment = UIStackViewAlignmentFill;
+    stackView.spacing = 8;
     
-    [self.view addSubview:stackView];
+    [contentView addSubview:stackView];
+    
+    [NSLayoutConstraint constraintWithItem:stackView
+                        attribute:NSLayoutAttributeLeading
+                        relatedBy:NSLayoutRelationEqual
+                        toItem:contentView
+                        attribute:NSLayoutAttributeLeading
+                        multiplier:1.0
+                        constant:8.0].active = YES;
+    [NSLayoutConstraint constraintWithItem:stackView
+                        attribute:NSLayoutAttributeTop
+                        relatedBy:NSLayoutRelationEqual
+                        toItem:contentView
+                        attribute:NSLayoutAttributeTop
+                        multiplier:1.0
+                        constant:8.0].active = YES;
+    [NSLayoutConstraint constraintWithItem:contentView
+                        attribute:NSLayoutAttributeTrailing
+                        relatedBy:NSLayoutRelationEqual
+                        toItem:stackView
+                        attribute:NSLayoutAttributeTrailing
+                        multiplier:1.0
+                        constant:8.0].active = YES;
+    [NSLayoutConstraint constraintWithItem:contentView
+                        attribute:NSLayoutAttributeBottom
+                        relatedBy:NSLayoutRelationEqual
+                        toItem:stackView
+                        attribute:NSLayoutAttributeBottom
+                        multiplier:1.0
+                        constant:8.0].active = YES;
+    stackView.translatesAutoresizingMaskIntoConstraints = NO;
     
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (UIView *)createShopView {
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor whiteColor];
+    [view.heightAnchor constraintEqualToConstant:100].active = YES;
+    return view;
 }
-*/
+
+- (UIView *)createProductsView {
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor whiteColor];
+    [view.heightAnchor constraintEqualToConstant:100].active = YES;
+    return view;
+}
+
+- (UIView *)createAddressView {
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor whiteColor];
+    [view.heightAnchor constraintEqualToConstant:100].active = YES;
+    return view;
+}
+
+- (UIView *)createPictureView {
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor whiteColor];
+    [view.heightAnchor constraintEqualToConstant:100].active = YES;
+    return view;
+}
+
+- (UIView *)createClientView {
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor whiteColor];
+    [view.heightAnchor constraintEqualToConstant:100].active = YES;
+    return view;
+}
+
+- (UIView *)createNotesView {
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor whiteColor];
+    [view.heightAnchor constraintEqualToConstant:100].active = YES;
+    return view;
+}
 
 @end
