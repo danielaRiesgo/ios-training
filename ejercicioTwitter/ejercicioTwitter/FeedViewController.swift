f4ef1e7b5d95b694a8987feb475c6da16319f6cf
danielaRiesgo <danielap.riesgo@gmail.com>
2016-02-11 15:25:15 -0300

    Instaladas las dependencias. Puestas los componentes mínimos de la interfaz

---
+++ ejercicioTwitter/ejercicioTwitter/FeedViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FeedViewController.swift
+//  ejercicioTwitter
+//
+//  Created by Daniela Riesgo on 2/11/16.
+//  Copyright © 2016 Daniela Riesgo. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
